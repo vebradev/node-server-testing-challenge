a12cf1ee954a5d99862be5493357d17f44c24349
vebradev <justinas@vebra.dev>
2019-07-25 17:58:36 +0300

    Add server.spec.js to test endpoint

--- server.js
+++ server.js
@@ -1,10 +1,36 @@
-const express = require('express');
+const express = require("express");
+
+const db = require("./users/users-model");
 
 const server = express();
 server.use(express.json());
 
 server.get("/", (req, res) => {
-  res.json("On");
+  res.status(200).json({
+    message: "✅"
+  });
+});
+
+server.get("/api/users", (req, res) => {
+  db.getAllUsers()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+server.post("/api/users", (req, res) => {
+  const user = req.body;
+
+  db.addUser(user)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(error => {
+      res.json(error);
+    });
 });
 
 const port = process.env.PORT || 4123;
--- users/users-model.js
+++ users/users-model.js
@@ -11,12 +11,13 @@ function getAllUsers() {
   return db("users").select("*");
 }
 
-function getSingleUser() {
-
+function getSingleUser(id) {
+  return db("users").select("username").where({ id });
 }
 
-function addUser() {
-
+async function addUser(user) {
+  const [id] = await db("users").insert(user);
+  return getSingleUser(id);
 }
 
 function removeUser() {
